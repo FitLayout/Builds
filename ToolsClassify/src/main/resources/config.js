e18e378ea5fbeda48d027994b4842d3b08674b84
Radek Burget <burgetr@fit.vutbr.cz>
2016-02-08 16:03:31 +0100

    Project-specific annotator configuration

--- ToolsClassify/src/main/resources/config.js
+++ ToolsClassify/src/main/resources/config.js
@@ -1,3 +1,10 @@
 //common project-specific configuration
 
 println("WELCOME!");
+
+//configure the Annotator plugin GUI
+//the default tag type for the assigned tags
+annotator.tagType = "FitLayout.Annotate";
+
+//the tag list
+annotator.tags = ["h1", "h2", "h3", "paragraph"];
