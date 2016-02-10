d862697e900ad94ee7defdad213a5ca06e26c562
Radek Burget <burgetr@fit.vutbr.cz>
2016-02-10 13:58:45 +0100

    Default segmentation config

--- ToolsClassify/src/main/resources/config.js
+++ ToolsClassify/src/main/resources/config.js
@@ -2,6 +2,19 @@
 
 println("WELCOME!");
 
+//CSSBox default redndering parametres
+proc.setServiceParams("FitLayout.CSSBox", {
+	width: 1200,
+	height: 800,
+	useVisualBounds: false
+});
+
+//Segmentation parametres
+//see default_operators.js for the operators
+proc.setServiceParams("FitLayout.Grouping", {
+	preserveAuxAreas: true
+});
+
 //configure the Annotator plugin GUI
 //the default tag type for the assigned tags
 annotator.tagType = "FitLayout.Annotate";
