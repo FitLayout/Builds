be0384d013ec8a4be06db16c72ea0ea1a1b22578
Radek Burget <burgetr@fit.vutbr.cz>
2016-02-15 18:12:59 +0100

    Training data extraction fixes

--- ToolsClassify/src/main/resources/console_init.js
+++ ToolsClassify/src/main/resources/console_init.js
@@ -2,6 +2,8 @@
 
 println("Useful commands:");
 println("proc.execInternal('create_training_set.js')  -- creates a sample training set containing a few pages");
+println("proc.execInternal('extract_training_data.js')  -- creates a training ARFF file from the training set");
 
 //connect the storage immediately for testing
 storage.connect("sesame:http://localhost:8080/openrdf-sesame/repositories/user");
+//storage.connect("blazegraph:http://localhost:8080/blazegraph");
