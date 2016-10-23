ea3febd59b6fd2a497f4bfcafa8760b3e3ad6d22
Radek Burget <burgetr@fit.vutbr.cz>
2016-10-23 13:36:49 +0200

    ToolsSegmentation added

---
+++ ToolsSegmentation/README.md
@@ -0,0 +1,5 @@
+FITLayout Page Segmentation Tools 
+=================================
+(c) 2016 Radek Burget (burgetr@fit.vutbr.cz)
+
+This is an experimental FITLayout build that is used for testing different page segmentation algorithms.
---
+++ ToolsSegmentation/src/main/java/org/fit/layout/build/segm/ConsoleSegm.java
@@ -0,0 +1,25 @@
+/**
+ * ConsoleTest.java
+ *
+ * Created on 13. 1. 2016, 23:28:24 by burgetr
+ */
+package org.fit.layout.build.segm;
+
+import org.fit.layout.tools.Console;
+
+/**
+ * 
+ * @author burgetr
+ */
+public class ConsoleSegm extends Console
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        Console.main(args);
+    }
+
+}
