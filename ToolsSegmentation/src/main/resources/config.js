ce4091b3ff4cce49ffaf694a1e6514ec05477d1c
Radek Burget <burgetr@fit.vutbr.cz>
2016-10-25 16:15:47 +0200

    Browser configuration

---
+++ ToolsSegmentation/src/main/resources/config.js
@@ -0,0 +1,10 @@
+//common project-specific configuration
+
+println("SEGM");
+
+//CSSBox default redndering parametres
+proc.setServiceParams("FitLayout.CSSBox", {
+	width: 1200,
+	height: 800,
+	useVisualBounds: false
+});
