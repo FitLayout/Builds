15614e94319b839ba8d8d2a60d3d4ed614f31a00
Radek Burget <burgetr@fit.vutbr.cz>
2019-09-30 15:07:25 +0200

    Obsolete builds removed

--- ToolsTimes/src/main/resources/config.js
+++
@@ -1,8 +0,0 @@
-//common project-specific configuration
-
-println("WELCOME!");
-
-entities.clearTaggers();
-entities.addTagger(entities.findTagger('FITLayout.Tag.Hour', {}));
-entities.addTagger(entities.findTagger('FITLayout.Tag.Minute', {}));
-//entities.addTagger(entities.findTagger('FITLayout.Tag.Location', {}));
